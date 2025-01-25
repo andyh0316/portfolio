b9b2bac36c03ebac0452134b856944fc7bde881f
Andy Hong <andyhong0316@yahoo.com>
2025-01-25 10:48:25 -0800

    update

--- src/app/resume/components/Experience/CobroConsulting/Features.tsx
+++ src/app/resume/components/Experience/CobroConsulting/Features.tsx
@@ -1,26 +1,49 @@
-import { Button, Chip, Dialog, Grid, Stack } from "@/components";
+import { Button, Chip, Dialog, Grid, Stack, Typography, Box } from "@/components";
 import { ReactNode, useState } from "react";
 import { YoutubePlayer } from "../../common/YoutubePlayer";
+import { Breakpoint } from "@mui/material";
 
 type Feature = "Data Import" | "Reports" | "Texting";
 interface FeatureItem {
   feature: Feature;
   content: ReactNode;
+  dialogMaxWidth: Breakpoint;
 }
 
-export const Features = () => {
-  const featureItems: FeatureItem[] = [
-    {
-      feature: "Data Import",
-      content: (
-        <Stack spacing={1}>
-          <YoutubePlayer videoId="ggRSyhMAxuo" />
+const featureItems: FeatureItem[] = [
+  {
+    feature: "Data Import",
+    dialogMaxWidth: "lg",
+    content: (
+      <Stack spacing={1}>
+        <YoutubePlayer videoId="JVDRc7ui_sU" />
 
-          <Stack>Tech used: ReactJS, MUI, .NET, Websocket SignalR, Async processes, Excel parsing, AI</Stack>
+        <Stack>Tech used: ReactJS, MUI, .NET, Websocket SignalR, Async processes, Excel parsing, AI</Stack>
+      </Stack>
+    ),
+  },
+  {
+    feature: "Texting",
+    dialogMaxWidth: "md",
+    content: (
+      <Stack spacing={2}>
+        <Box border={1} borderColor="divider">
+          <img src="/texting.png" alt="Texting" width="100%" style={{ display: "block" }} />
+        </Box>
+
+        <Stack spacing={0.5}>
+          <Typography variant="inherit">Features for managing text messages to students.</Typography>
+          <Typography variant="inherit">Can automate mass sending, scheduled sending and automatic replies.</Typography>
+          <Typography variant="inherit">
+            Greatly increases the effciency of communication between staff and students.
+          </Typography>
         </Stack>
-      ),
-    },
-  ];
+      </Stack>
+    ),
+  },
+];
+
+export const Features = () => {
   const [openedFeatureItem, setOpenedFeatureItem] = useState<FeatureItem>();
 
   return (
@@ -43,11 +66,10 @@ export const Features = () => {
         {openedFeatureItem && (
           <Dialog
             open={true}
-            title="Haha"
-            content={openedFeatureItem.content}
             width="90%"
-            maxWidth="lg"
-            // width="1000px"
+            title={openedFeatureItem.feature}
+            content={openedFeatureItem.content}
+            maxWidth={openedFeatureItem.dialogMaxWidth}
             footer={
               <Button variant="outlined" onClick={() => setOpenedFeatureItem(undefined)}>
                 Close
